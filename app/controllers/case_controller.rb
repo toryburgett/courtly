6d333ea212570324f1c946c53bc9b1e70b21f3ed
Tory Burgett <vdburgett@gmail.com>
2015-11-16 17:30:44 -0500

    Added stuff to case_controller.rb and seeds.rb

--- app/controllers/case_controller.rb
+++ app/controllers/case_controller.rb
@@ -1,13 +1,47 @@
 class CaseController < ApplicationController
   def index
+    @case = Case.all
   end
 
   def show
+    @case = Case.find(params[:id])
   end
 
   def new
+    @case = Case.new
   end
 
   def edit
+    @case = Case.find(params[:id])
   end
+
+  def create
+    @case = Case.new(case_params)
+    if @case.save
+      redirect_to @case
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @case = Case.find(params[:id])
+    if @case.update(case_params)
+      redirect_to @case
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @case = Case.find(params[:id])
+    @case.destroy
+    redirect_to cases_path()
+  end
+
+  private
+  def article_params
+    params.require(:case).permit(:plaintiff, :defendant, :opiniondate, :argument, :description, :judgement)
+  end
+
 end
