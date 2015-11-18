96c44e0b977504c5658781958887dd61ec9f1fa0
Tory Burgett <vdburgett@gmail.com>
2015-11-18 10:53:53 -0500

    Columns to opinions and justice migrations, justice seed data

--- db/migrate/20151117204859_create_justices.rb
+++ db/migrate/20151117204859_create_justices.rb
@@ -2,7 +2,10 @@ class CreateJustices < ActiveRecord::Migration
   def change
     create_table :justices do |t|
       t.string :name
-
+      t.string :full_name
+      t.string :title
+      t.text :bio
+      t.string :photo_url
       t.timestamps null: false
     end
   end
--- db/migrate/20151117205547_create_opinions.rb
+++ db/migrate/20151117205547_create_opinions.rb
@@ -4,7 +4,43 @@ class CreateOpinions < ActiveRecord::Migration
       t.references :case, index:true, foreign_key: true
       t.references :justice, index:true, foreign_key: true
 
-      t.string :ruling
+      # MAJORITY OPINION
+      t.integer :majority_with
+
+      t.integer :majority_wrote
+      t.integer :majority_joined
+      t.integer :majority_joined_full
+      t.integer :majority_joined_part
+
+      t.integer :concurrance_wrote
+      t.integer :concurrance_number
+      t.integer :concurrance_joined
+      t.integer :concurrance_joined_full
+      t.integer :concurrance_joined_part
+      t.integer :concurrance_joined_number
+
+      # DISSENTING OPINION
+      t.integer :dissent_with
+
+      t.integer :dissent_wrote
+      t.integer :dissent_number
+      t.integer :dissent_joined
+      t.integer :dissent_joined_full
+      t.integer :dissent_joined_part
+      t.integer :dissent_joined_number
+
+      t.integer :con_dissent_wrote
+      t.integer :con_dissent_number
+      t.integer :con_dissent_joined
+      t.integer :con_dissent_joined_full
+      t.integer :con_dissent_joined_part
+      t.integer :con_dissent_joined_number
+
+      # DID NOT PARTICIPATE IN VOTE
+      t.integer :no_part
+
+      # JUST IN CASE! For the sake of math...
+      t.integer :num_opinions_signed
 
       t.timestamps null: false
     end
