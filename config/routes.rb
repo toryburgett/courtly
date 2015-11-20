79cb6562f3bb164eac623084c3224650ca8b2fa9
Tory Burgett <vdburgett@gmail.com>
2015-11-20 09:37:31 -0500

    Added home and about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'cases#index'
+  root 'welcome#index'
+  resources :about, only: [:index]
+
   resources :justices do
     resources :opinions
   end
