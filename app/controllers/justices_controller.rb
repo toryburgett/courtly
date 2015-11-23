2283ac120f7c3dadd17daa1bd5695c444bb01527
Tory Burgett <vdburgett@gmail.com>
2015-11-23 13:43:33 -0500

    More changes

--- app/controllers/justices_controller.rb
+++ app/controllers/justices_controller.rb
@@ -5,6 +5,25 @@ class JusticesController < ApplicationController
 
   def show
     @justice = Justice.find(params[:id])
+    @opinions = @justice.opinions
+    @wrote_majority = 0
+    @wrote_dissent = 0
+    @majoirty_votes_total = 0
+    @dissent_votes_total = 0
+    @opinions.each do |t|
+      if t.majority_wrote == 1
+        @wrote_majority = @wrote_majority + 1
+      end
+      if t.dissent_wrote == 1
+        @wrote_dissent = @wrote_dissent + 1
+      end
+      if t.majority_with == 1
+        @majoirty_votes_total = @majoirty_votes_total + 1
+      end
+      if t.dissent_with == 1
+        @dissent_votes_total = @dissent_votes_total + 1
+      end
+    end
   end
 
   def new
