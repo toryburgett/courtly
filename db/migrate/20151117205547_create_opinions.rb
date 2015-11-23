4f80a3990618440f24a9ef6a4de7ab013f317fbe
Tory Burgett <vdburgett@gmail.com>
2015-11-23 14:33:32 -0500

    Now with graphs on judges pagesgit add .

--- db/migrate/20151117205547_create_opinions.rb
+++ db/migrate/20151117205547_create_opinions.rb
@@ -3,15 +3,44 @@ class CreateOpinions < ActiveRecord::Migration
     create_table :opinions do |t|
       t.references :case, index:true, foreign_key: true
       t.references :justice, index:true, foreign_key: true
-
+      t.string :sided, default: "No Part"
       t.integer :majority_with, default: 0
       t.integer :majority_wrote, default: 0
 
       t.integer :dissent_with, default: 0
       t.integer :dissent_wrote, default: 0
+      t.integer :dissent_roberts_join, default: 0
+      t.integer :dissent_scalia_join, default: 0
+      t.integer :dissent_kennedy_join, default: 0
+      t.integer :dissent_thomas_join, default: 0
+      t.integer :dissent_ginsburg_join, default: 0
+      t.integer :dissent_breyer_join, default: 0
+      t.integer :dissent_alito_join, default: 0
+      t.integer :dissent_sotomayor_join, default: 0
+      t.integer :dissent_kagan_join, default: 0
 
       t.integer :concurrance_wrote, default: 0
+      t.integer :concurrance_roberts_join, default: 0
+      t.integer :concurrance_scalia_join, default: 0
+      t.integer :concurrance_kennedy_join, default: 0
+      t.integer :concurrance_thomas_join, default: 0
+      t.integer :concurrance_ginsburg_join, default: 0
+      t.integer :concurrance_breyer_join, default: 0
+      t.integer :concurrance_alito_join, default: 0
+      t.integer :concurrance_sotomayor_join, default: 0
+      t.integer :concurrance_kagan_join, default: 0
+
+
       t.integer :condis_wrote, default: 0
+      t.integer :condis_roberts_join, default: 0
+      t.integer :condis_scalia_join, default: 0
+      t.integer :condis_kennedy_join, default: 0
+      t.integer :condis_thomas_join, default: 0
+      t.integer :condis_ginsburg_join, default: 0
+      t.integer :condis_breyer_join, default: 0
+      t.integer :condis_alito_join, default: 0
+      t.integer :condis_sotomayor_join, default: 0
+      t.integer :condis_kagan_join, default: 0
 
       t.integer :no_part, default: 0
 
--- db/schema.rb
+++ db/schema.rb
@@ -39,15 +39,43 @@ ActiveRecord::Schema.define(version: 20151119183040) do
   create_table "opinions", force: :cascade do |t|
     t.integer  "case_id"
     t.integer  "justice_id"
-    t.integer  "majority_with",     default: 0
-    t.integer  "majority_wrote",    default: 0
-    t.integer  "dissent_with",      default: 0
-    t.integer  "dissent_wrote",     default: 0
-    t.integer  "concurrance_wrote", default: 0
-    t.integer  "condis_wrote",      default: 0
-    t.integer  "no_part",           default: 0
-    t.datetime "created_at",                    null: false
-    t.datetime "updated_at",                    null: false
+    t.string   "sided",                      default: "No Part"
+    t.integer  "majority_with",              default: 0
+    t.integer  "majority_wrote",             default: 0
+    t.integer  "dissent_with",               default: 0
+    t.integer  "dissent_wrote",              default: 0
+    t.integer  "dissent_roberts_join",       default: 0
+    t.integer  "dissent_scalia_join",        default: 0
+    t.integer  "dissent_kennedy_join",       default: 0
+    t.integer  "dissent_thomas_join",        default: 0
+    t.integer  "dissent_ginsburg_join",      default: 0
+    t.integer  "dissent_breyer_join",        default: 0
+    t.integer  "dissent_alito_join",         default: 0
+    t.integer  "dissent_sotomayor_join",     default: 0
+    t.integer  "dissent_kagan_join",         default: 0
+    t.integer  "concurrance_wrote",          default: 0
+    t.integer  "concurrance_roberts_join",   default: 0
+    t.integer  "concurrance_scalia_join",    default: 0
+    t.integer  "concurrance_kennedy_join",   default: 0
+    t.integer  "concurrance_thomas_join",    default: 0
+    t.integer  "concurrance_ginsburg_join",  default: 0
+    t.integer  "concurrance_breyer_join",    default: 0
+    t.integer  "concurrance_alito_join",     default: 0
+    t.integer  "concurrance_sotomayor_join", default: 0
+    t.integer  "concurrance_kagan_join",     default: 0
+    t.integer  "condis_wrote",               default: 0
+    t.integer  "condis_roberts_join",        default: 0
+    t.integer  "condis_scalia_join",         default: 0
+    t.integer  "condis_kennedy_join",        default: 0
+    t.integer  "condis_thomas_join",         default: 0
+    t.integer  "condis_ginsburg_join",       default: 0
+    t.integer  "condis_breyer_join",         default: 0
+    t.integer  "condis_alito_join",          default: 0
+    t.integer  "condis_sotomayor_join",      default: 0
+    t.integer  "condis_kagan_join",          default: 0
+    t.integer  "no_part",                    default: 0
+    t.datetime "created_at",                                     null: false
+    t.datetime "updated_at",                                     null: false
   end
 
   add_index "opinions", ["case_id"], name: "index_opinions_on_case_id", using: :btree
